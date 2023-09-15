2957bf7a8fe8bd6b99c419099922fbdbe674bba9
raj <mailmeraj23@gmail.com>
2023-09-14 22:30:17 -0500

    intial commit with models and setting up a project1

--- poller/main/views.py
+++ poller/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from main import models
 
 # Create your views here.
+from django.views.generic import (
+    ListView 
+)
+
+class 
--- poller/poller/settings.py
+++ poller/poller/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main'
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
